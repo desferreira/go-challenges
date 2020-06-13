1c324db7d30dc5919e52748c2f8c4c6998853351
Diego <diego.ferreira@justa.com.vc>
2020-06-13 13:41:38 -0300

    fix: :bug: Corrige erro em main.go
    Signed-off-by: Diego <diego.ferreira@justa.com.vc>

--- urlshortner/main/main.go
+++ urlshortner/main/main.go
@@ -17,13 +17,25 @@ type RedirectList struct {
 	Redirects []Redirect `json:"list"`
 }
 
+func main() {
+	http.HandleFunc("/", handle)
+
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Println("Failed to build server")
+	}
+}
+
 func handle(w http.ResponseWriter, r *http.Request) {
 	redirects := parserJSONMap()
+	fmt.Println(r.URL.Path[1:])
+	fmt.Println(strings.TrimSpace(r.URL.Path[1:]))
+	fmt.Println(redirects[strings.TrimSpace(r.URL.Path[1:])])
 	http.Redirect(w, r, redirects[strings.TrimSpace(r.URL.Path[1:])], 301)
 }
 
 func parserJSONMap() map[string]string {
-	jsonMapFile, err := ioutil.ReadFile("config/map.json")
+	jsonMapFile, err := ioutil.ReadFile("../config/map.json")
 	if err != nil {
 		fmt.Printf("Failed to open %v \n", err)
 	}
