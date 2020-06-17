ae5a2697c2e561edb4e24e5f5ea73a97fe151c8a
Diego <diego.ferreira@justa.com.vc>
2020-06-16 22:59:31 -0300

    refactor: :hammer: Adiciona tratamento de erros e funções auxiliares
    Signed-off-by: Diego <diego.ferreira@justa.com.vc>

--- task/db/tasks.go
+++ task/db/tasks.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -15,6 +16,9 @@ type Task struct {
 	Value string
 }
 
+/*
+Create a bucket to store the tasks
+*/
 func Init(dbPath string) error {
 	var err error
 	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
@@ -25,4 +29,38 @@ func Init(dbPath string) error {
 		_, err := tx.CreateBucketIfNotExists(tasksBucket)
 		return err
 	})
+}
+
+/*
+Register a task on the bucket
+*/
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(tasksBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := iotb(id)
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, err
+}
+
+/*
+Convert int to []byte
+*/
+func iotb(num int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(num))
+	return b
+}
+
+/*
+Convert []byte to int
+*/
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
 }
